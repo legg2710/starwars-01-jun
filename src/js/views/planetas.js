1531a70e15501fc5578820cb0a78003789c71ebe
legg2710 <luisemilio2710@gmail.com>
2021-07-02 16:02:00 +0000

    Starwars 20% Done

---
+++ src/js/views/planetas.js
@@ -0,0 +1,23 @@
+import React, { useContext, useEffect, useState } from "react";
+
+export const Planetas = () => {
+    const {store, actions}= useContext(Context);
+    
+    // let [dataPlanetas,setDataPlanetas] = useState([]);
+
+    // const planetList = () => {
+    //     fetch("url",{
+    //         method:"GET"
+    //     })
+    //     .then(response => response.json())
+    //     .then(data => setDataPlanetas(data.results));
+    // };
+
+
+    useEffect(()=>{
+
+        actions.fetchPlanetas();
+    }, []);
+    
+    return ()
+};
